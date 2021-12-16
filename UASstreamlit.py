82174ae2d57a9f200b9c79caf3670c67605ab2ae
bernardsusanto <52168411+bernardsusanto@users.noreply.github.com>
2021-12-16 15:20:21 +0700

    Update UASstreamlit.py

--- UASstreamlit.py
+++ UASstreamlit.py
@@ -179,7 +179,7 @@ for negara in data:                 #loop pada tiap negara di file json
         st.write("Subregion Negara: ",subregionterkecil)
         
 #Negara Produksi terkecil kumulatif
-datasoal3k = datasoal3[datasoal3["Kumulatif Produksi"]!=0]    #Menghilangkan nilai 0 pada data kumulatif produksi
+datasoal3k = datasoal3[datasoal3["Kumulatif_Produksi"]!=0]    #Menghilangkan nilai 0 pada data kumulatif produksi
 datasoal3k = datasoal3k.tail(1)               #Mencari negara dengan produksi kumulatif terkecil
 datakterkecil = datasoal3k["Kode Negara"]       #Menentukan kode negaranya
 kodetercari3 = datakterkecil.values[0]      #Mendapat kode negara dengan kumulatif produksi terkecil
