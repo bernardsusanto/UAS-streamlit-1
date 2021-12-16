c0cbf97e89c1045b95683836240f151d21eba98d
bernardsusanto <52168411+bernardsusanto@users.noreply.github.com>
2021-12-16 17:56:21 +0700

    Update UASstreamlit.py

--- UASstreamlit.py
+++ UASstreamlit.py
@@ -50,7 +50,7 @@ st.pyplot(fig)
 
 #Soal 2
 st.subheader("Grafik Produksi Terbesar Minyak Mentah Negara pada Tahun Tertentu")
-Bnegara = st.slider("Masukkan berapa besar negara: ",min_value=1,max_value=200)
+Bnegara = st.slider("Masukkan berapa besar negara: ",min_value=1,max_value=20)
 tahuninput = st.number_input("Masukkan tahun (1971-2015): ",min_value=1971,max_value=2015)
 
 df_indexed = df.set_index("tahun")              #Mengubah index menjadi tahun
@@ -69,7 +69,7 @@ st.pyplot(fig)
 
 #Soal2b
 st.subheader("Grafik Produksi Terkecil Minyak Mentah Negara pada Tahun Tertentu")
-Bnegarakecil = st.slider("Masukkan jumlah negara terendah: ",min_value=1,max_value=200)
+Bnegarakecil = st.slider("Masukkan jumlah negara terendah: ",min_value=1,max_value=20)
 tahuninputkecil = st.number_input("Masukkan tahun  (1971-2015): ",min_value=1971,max_value=2015)
 
 dftahun2b = df_indexed[df_indexed["produksi"]!=0]    #Menghilangkan 0 pada data produksi
@@ -86,7 +86,7 @@ st.pyplot(fig)
 
 #Soal 3
 st.subheader("Grafik Produksi Kumulatif Minyak Mentah B-Besar Negara")
-Bnegarak = st.slider("Masukkan jumlah besar negara: ",min_value=1,max_value=200)
+Bnegarak = st.slider("Masukkan jumlah besar negara: ",min_value=1,max_value=20)
 
 listkodenegara1 = list(dftahun["kode_negara"])  #Mengambil deretan kode negara dari data di soal sebelumnya
                                                 #Dikarenakan ketiadaan negara Afghanistan pada file csv yang diberikan
