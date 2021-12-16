1fab523e629c741d7a77707052fb0879a338c94b
bernardsusanto <52168411+bernardsusanto@users.noreply.github.com>
2021-12-16 18:40:20 +0700

    Update UASstreamlit.py

--- UASstreamlit.py
+++ UASstreamlit.py
@@ -51,7 +51,7 @@ st.pyplot(fig)
 #Soal 2
 st.subheader("Grafik Produksi Terbesar Minyak Mentah Negara pada Tahun Tertentu")
 Bnegara = st.slider("Masukkan berapa besar negara: ",min_value=1,max_value=20)
-tahuninput = st.selectbox("Masukkan tahun (1971-2015): ",('1971','1972',1973','1974','1975','1976','1977','1978','1979','1980','1981','1982','1983','1984','1985','1986','1987','1988','1989','1990','1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015'))
+tahuninput = st.selectbox("Masukkan tahun (1971-2015): ",('1971','1972','1973','1974','1975','1976','1977','1978','1979','1980','1981','1982','1983','1984','1985','1986','1987','1988','1989','1990','1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015'))
 
 df_indexed = df.set_index("tahun")              #Mengubah index menjadi tahun
 dftahun = df_indexed.loc[int(tahuninput)]       #Men-slice data hanya sesuai input tahun dari user
